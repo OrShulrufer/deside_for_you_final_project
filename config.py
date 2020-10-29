0432ba868914d9b15f395b9739ebfa92f9eaef27
Or Shulrufer <pumaor23@gmail.com>
2020-10-29 09:43:46 +0200

    or

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DEBUG = False
 MY_SQL_CONNECTOR_PATH = "jars/mysql-connector-java-5.1.48.jar"
 SPARK_LOG_CONF = 'true'
-DB_URL = "remotemysql.com:3306/fHZDwIzYH2"
-DB_USER = "fHZDwIzYH2"
-DB_PASSWORD = "ZSMUnFWc3b"
+DB_URL = ""
+DB_USER = ""
+DB_PASSWORD = ""
